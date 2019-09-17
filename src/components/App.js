7b7a92d3a36ea78ba75afa349d3b13cf2ed5dbe9
Carlos Us Santiago <cdus12@gmail.com>
2019-09-17 00:00:59 -0700

    updated test

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import { 
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
-import Home from '../containers/Home';
-import NavBar from './navBar/NavBar';
 import Landing from './landing/Landing';
 
 export default function App() {
